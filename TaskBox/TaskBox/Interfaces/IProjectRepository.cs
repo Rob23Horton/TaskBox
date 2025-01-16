f434527f56d4c5848010f032e13319dbe2017e48
Rob <rob23horton@gmail.com>
2025-01-16 20:31:58 +0000

    Added Gantt Chart/Table to Home Page and ability to create projects.
    Added CreateProject to ProjectController and Repository
    
    Made Gantt Chart go to today on initalisation
    
    Deleted unneeded StateHasChanged in Segment Page
    
    Added ProjectAndUser to Shared

--- TaskBox/TaskBox/Interfaces/IProjectRepository.cs
+++ TaskBox/TaskBox/Interfaces/IProjectRepository.cs
@@ -4,6 +4,7 @@ namespace TaskBox.Interfaces
 {
 	public interface IProjectRepository
 	{
+		public ApiResponse CreateProject(int UserId, Project Project);
 		public bool CheckProjectPermission(int UserId, int ProjectId);
 		public ProjectUserPermission GetProjectUserPermission(int UserId, int ProjectId);
 		public Project GetProject(int ProjectId);
